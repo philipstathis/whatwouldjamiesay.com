601b5ad58077a44a45d664814144c89794553e2a
Philip Stathis <1742750+philipstathis@users.noreply.github.com>
2019-05-14 14:31:49 -0400

    Add files via upload

---
+++ run.js
@@ -0,0 +1,5 @@
+var items = ["Cooodyyyy Rrrrrooooosssseeeee :musical_note:", "Joooon Raaa - Chenkooooo", "Just spark the scala using Kafka", "Why did we hire him again?", "I'm feeling Gussy's", ":popcorn:", "It's a dump in a box", "It's a polished turd", "It's a tire fire", "One bad data point can bring down the entire application", "Neither Useful Nor Ornamental", "His allocation is 75% not giving a fuck, and 25% giving fewer fucks"];
+
+var item = items[Math.floor(Math.random()*items.length)];
+
+document.write('<h1 class="almost">' + item + '</h1>');
