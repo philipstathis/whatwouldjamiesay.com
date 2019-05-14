480d93109eac914f75365d2a81ce96c1d8db31e9
Philip Stathis <1742750+philipstathis@users.noreply.github.com>
2019-05-14 17:05:15 -0400

    Update run.js

--- run.js
+++ run.js
@@ -1,10 +1,27 @@
-var items = ["🎵 Cooodyyyy Rrrrrooooosssseeeee 🎵", "🎵 Joooon Raaa - Chenkooooo 🎵", "Just spark the scala using Kafka",
-             "Why did we hire him again?", "I'm feeling Gussy's", ":popcorn:", "It's a dump in a box", "It's a polished turd",
-             "It's a tire fire", "One bad data point can bring down the entire application", "Neither Useful Nor Ornamental",
-             "His allocation is 75% not giving a fuck, and 25% giving fewer fucks", "...fucknuts...", "Hey Buckaroo!",
-             "👎", "We should take credit for it", "Hello my lovelies!", "(scottish) Take it out back and bash in the head with a saddle",
-             "Good riddance to bad rubbish", "You really treasury-dev ed that joke", "(☞ ͡° ͜ʖ ͡°)☞"];
+const quotes = [
+  '🎵 Cooodyyyy Rrrrrooooosssseeeee 🎵',
+  '🎵 Joooon Raaa - Chenkooooo 🎵',
+  'Just spark the scala using Kafka',
+  'Why did we hire him again?',
+  "I'm feeling Gussy's",
+  ':popcorn:',
+  "It's a dump in a box",
+  "It's a polished turd",
+  "It's a tire fire",
+  'One bad data point can bring down the entire application',
+  'Neither Useful Nor Ornamental',
+  'His allocation is 75% not giving a fuck, and 25% giving fewer fucks',
+  '...fucknuts...',
+  'Hey Buckaroo!',
+  '👎',
+  'We should take credit for it',
+  'Hello my lovelies!',
+  '(scottish) Take it out back and bash in the head with a saddle',
+  'Good riddance to bad rubbish',
+  'You really treasury-dev ed that joke',
+  '(☞ ͡° ͜ʖ ͡°)☞'
+];
 
-var item = items[Math.floor(Math.random()*items.length)];
-
-document.write('<h1 class="almost">' + item + '</h1>');
+export default function getQuote() {
+    return quotes[Math.trunc(Math.random() * quotes.length)]
+};
